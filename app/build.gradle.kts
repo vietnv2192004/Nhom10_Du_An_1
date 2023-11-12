a75df07484084abcf47eab9820d629a8f1bf1537
DuAn1_Nhom10 <vietnvph34895@fpt.edu.vn>
2023-11-12 20:00:07 +0700

    sua phien ban len 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.nhom10_duan1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.nhom10_duan1"
